0763d95bfb69ae0a3094ea5dcf0da2a3435fe30f
Ben <benjamin.a.spencer@gmail.com>
2016-06-14 15:48:25 -0400

    starts Message Component

--- Clack/components/Messages.js
+++ Clack/components/Messages.js
@@ -3,6 +3,29 @@ var ReactDOM = require('react-dom');
 
 var Messages = React.createClass({
   render: function() {
+    return (
 
+      var text = message.text;
+      return (
+        <div key={i} className="message">
+          <a href={"https://twitter.com/" + message.name + "/"} target="_blank"><img src={"https://twitter.com/" + message.name + "/profile_image"} className="message_profile-pic" /></a>
+          <a href={"https://twitter.com/" + message.name + "/"} target="_blank" className="message_username">{message.name}</a>
+          <span className="message_timestamp">{message.time.toLocaleTimeString()}</span>
+          <span className="message_content" dangerouslySetInnerHTML={{__html: text}}></span>
+        </div>
+      )
+    })
+
+      <div id="message-list">
+        <div className="time-divide">
+          <span className="date">
+
+          </span>
+        </div>
+        {messageList}
+      </div>
+
+
+    )
   }
 })
